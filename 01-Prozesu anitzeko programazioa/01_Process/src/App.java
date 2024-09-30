36173be67105e88fc6b008cb2368628fd7c70a92
maiderarizmendiarrieta <arizmendiarrieta.maider@uni.eus>
2024-09-30 11:00:08 +0200

    Aldaketa batzuk

--- 01-Prozesu anitzeko programazioa/01_Process/src/App.java
+++ 01-Prozesu anitzeko programazioa/01_Process/src/App.java
@@ -1,8 +1,10 @@
 import java.io.InputStream;
 
 public class App {
+    @SuppressWarnings("deprecation")
     public static void main(String[] args) throws Exception {
 
+        // LEHENENGO MODUA
         try {
             System.out.println("Process-a sortzen...");
             String[] cmd = {"notepad.exe"};
@@ -18,5 +20,20 @@ public class App {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        // BIGARREN MODUA
+        Runtime r = Runtime.getRuntime(); 
+        String komandoa= "NOTEPAD"; 
+        Process p; 
+        try {
+            p = r.exec(komandoa); 
+            System.out.println("Semea martxan "+p.pid()+" zenbakiarekin.");
+        } 
+        catch (Exception e) { 
+            System.out.println("Akatsa: "+komandoa); 
+            e.printStackTrace(); 
+        } 
+        // Adibidea Windowsen agindu-lerroko agindua exekutatzeko 
+        // Runtime/exec erabilita
     }
 }
