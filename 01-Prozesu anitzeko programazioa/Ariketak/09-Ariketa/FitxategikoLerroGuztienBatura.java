87267a3e73ac333023814ee07657f01e32c0e92c
maiderarizmendiarrieta <arizmendiarrieta.maider@uni.eus>
2024-10-07 21:24:48 +0200

    Ariketa 9

--- 01-Prozesu anitzeko programazioa/Ariketak/09-Ariketa/FitxategikoLerroGuztienBatura.java
+++ 01-Prozesu anitzeko programazioa/Ariketak/09-Ariketa/FitxategikoLerroGuztienBatura.java
@@ -6,28 +6,22 @@ public class FitxategikoLerroGuztienBatura {
     // Parametroan fitxategiaren izena jasoko dugu
     public static int kalkulatuBatura(String fitxategiIzena) {
         int batura = 0;
-        File f = null;
-        FileReader fr = null;
-        BufferedReader br = null;
 
-        try {
-            f = new File(fitxategiIzena);
-            fr = new FileReader(f);
-            br = new BufferedReader(fr);
 
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File(fitxategiIzena)))) {
             String lerroa;
-            while ((lerroa = br.readLine()) != null) {
+            while ((lerroa = reader.readLine()) != null) {
                 try {
                     // Lerroa zenbakia bada, gehitu baturara
                     int zenbakia = Integer.parseInt(lerroa.trim());
                     batura += zenbakia;
                 } catch (NumberFormatException e) {
                     // Ez da zenbaki bat, jarraitu irakurtzen
+                    e.printStackTrace();
                 }
             }
+            reader.close();
 
-            br.close();
-            fr.close();
         } catch (IOException ioe) {
             // Fitxategia ezin bada irakurri, 0 bueltatuko dugu
             batura = 0;
