d54646c3498bd64c59f70cedd6483fcd916269d9
maiderarizmendiarrieta <arizmendiarrieta.maider@uni.eus>
2025-02-06 18:51:03 +0100

    repaso

--- 02-Hari anitzek programazioa/AzterketaMaiderArizmendiarrieta/src/azterketamaiderarizmendiarrieta/Pultsularia.java
+++ 02-Hari anitzek programazioa/AzterketaMaiderArizmendiarrieta/src/azterketamaiderarizmendiarrieta/Pultsularia.java
@@ -18,10 +18,15 @@ public class Pultsularia extends Thread {
     private int counterP1;
     private int counterP2;
     private final Random random = new Random(); // Tira egitea ausaz egiteko
+    
+    public Pultsularia () {
+        counterP1 = 0;
+        counterP2 = 0;
+    }
 
     @Override
     public void run() {
-        while(counterP1!=20 && counterP2!=-20) {
+        while(counterP1<20 && counterP2>-20) {
             // [0,4] bitarteko indarra -> gero hari p1-aren ausazko tira zenb hartu
             int tira = 0;
             while (tira == 0) {
@@ -31,15 +36,17 @@ public class Pultsularia extends Thread {
             if (tira > 0) {
                 System.out.println(">>>>>>>>>>>>>>>TIRA ALICEK");
                 this.counterP1 += tira;
+                System.out.println(tira + " " + counterP1 + " " + counterP2);
             } else {
                 System.out.println("TIRA BOBEK<<<<<<<<<<<<<<<");
                 this.counterP2 -= Math.abs(tira);
+                System.out.println(tira + " " + counterP1 + " " + counterP2);
             }
         }
         
-        if (counterP1==20) {
+        if (counterP1>=20) {
             System.out.println("HA GANADO DERECHA");
-        } else {
+        } else if (counterP2<=-20) {
             System.out.println("HA GANADO IZQUIERDA");
         }
     }
